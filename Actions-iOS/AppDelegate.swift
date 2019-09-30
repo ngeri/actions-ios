9adf95694d3ef15b4e634d71cebb570fdaa4ddb4
Gergely Nemeth <ngergo100@gmail.com>
2019-09-30 23:40:51 +0200

    An

--- Actions-iOS/AppDelegate.swift
+++ Actions-iOS/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool { // swiftlint:disable:this line_length
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 }
--- Dangerfile
+++ Dangerfile
@@ -1,6 +1,3 @@
-# Warn when there is a big PR
-warn("Big PR") if git.lines_of_code > 500
-
-swiftlint.config_file = '.swiftlint.yml'
-swiftlint.binary_path = 'swiftlint/swiftlint'
-swiftlint.lint_files inline_mode: true
+swiftlint.directory = "Actions-iOS"
+swiftlint.lint_all_files = true
+swiftlint.lint_files
